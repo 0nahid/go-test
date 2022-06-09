ad2a554689066e75069d49066cc5832f913edb07
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-10 01:51:27 +0600

    go working nicely

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World! This is just a test")
+	fmt.Println("Hello I am Nahid Hassan ")
+	fmt.Println("I am from CSE Department")
+	fmt.Println("My Identity Number id: 22103166")
+}
