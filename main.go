d0c18a4b2f153fac761823d1dcffefeea6e3598e
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-10 15:22:01 +0600

    single and multiline comment '

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
+// a program for showing just how to use the Println function
 package main
 
 import "fmt"
 
+/*
+Author : Nahid
+Date : Friday Jun 10 ,2022
+*/
 func main() {
-	fmt.Println("Hello, World! This is just a test")
+	fmt.Println("Hello, World! \nThis is just a test")
 	fmt.Println("Hello I am Nahid Hassan ")
-	fmt.Println("I am from CSE Department")
+	fmt.Println("I am from CSE \tDepartment")
 	fmt.Println("My Identity Number id: 22103166")
 }
