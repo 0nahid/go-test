c38eb2b0ec9666b99ff24fec47b25378e7a2dc10
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-10 15:38:38 +0600

    data types on GO

--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 
 import "fmt"
 
-/*
-Author : Nahid
-Date : Friday Jun 10 ,2022
-*/
 func main() {
-	fmt.Println("Hello, World! \nThis is just a test")
-	fmt.Println("Hello I am Nahid Hassan ")
-	fmt.Println("I am from CSE \tDepartment")
+	/**
+	** int 8 → 2 ^8 = 256 = -128 0 127
+	** int 16 → 2 ^16 = 65536 = -32768 0 32767
+	** unit8 → 2 ^8 = 256 (0-256)
+	** "Nahid Hassan" → String data type
+	** 123 → int data type
+	** true → bool data type
+	** 0.123 → float data type
+	** []int → slice data type
+	** map[string]int → map data type
+	** struct → struct data type
+	** func → func data type
+	** interface → interface data type
+	** chan int → chan data type
+	 */
+
+	// fmt.Println("Hello, World! \nThis is just a test")
+	// fmt.Println("Hello I am Nahid Hassan ")
+	// fmt.Println("I am from CSE \tDepartment")
 	fmt.Println("My Identity Number id: 22103166")
 }
